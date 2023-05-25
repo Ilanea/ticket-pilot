dfa94b74e6e18d7b0fbcfbb773a39efda183cc21
Ilanea <Ilanea@users.noreply.github.com>
2023-05-25 20:45:26 +0200

    initialize vaadin template

---
+++ src/main/java/com/example/application/data/repository/ContactRepository.java
@@ -0,0 +1,8 @@
+package com.example.application.data.repository;
+
+import com.example.application.data.entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+}
