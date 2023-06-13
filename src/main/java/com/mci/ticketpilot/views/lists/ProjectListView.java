7ec8c9bfc59093725f930c62f9c3b01c1a213bf6
Ilanea <Ilanea@users.noreply.github.com>
2023-06-13 20:05:08 +0200

    feat: notifications (#19)
    * Logoeinbindung
    
    * Notifkationen
    
    Push  up  Notifikationen bei User, Ticket und Projektänderungen
    
    * change notification positions
    
    * Update index.html
    
    ---------
    
    Co-authored-by: Judith <laglerjudith@gmail.com>

--- src/main/java/com/mci/ticketpilot/views/lists/ProjectListView.java
+++ src/main/java/com/mci/ticketpilot/views/lists/ProjectListView.java
@@ -7,6 +7,8 @@ import com.mci.ticketpilot.views.MainLayout;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -73,12 +75,22 @@ public class ProjectListView extends VerticalLayout {
         service.saveProject(event.getProject());
         updateList();
         closeEditor();
+        // Benachrichtigung erstellen und anzeigen
+        Notification notification = new Notification("Neues Projekt wurde erstellt", 3000);
+        notification.setPosition(Notification.Position.BOTTOM_END);
+        notification.addThemeVariants(NotificationVariant.LUMO_PRIMARY);
+        notification.open();
     }
 
     private void deleteProject(ProjectForm.DeleteEvent event) {
         service.deleteProject(event.getProject());
         updateList();
         closeEditor();
+        // Benachrichtigung erstellen und anzeigen
+        Notification notification = new Notification("Projekt wurde gelöscht", 3000);
+        notification.setPosition(Notification.Position.BOTTOM_END);
+        notification.addThemeVariants(NotificationVariant.LUMO_PRIMARY);
+        notification.open();
     }
 
     private void configureGrid() {
--- src/main/java/com/mci/ticketpilot/views/lists/TicketListView.java
+++ src/main/java/com/mci/ticketpilot/views/lists/TicketListView.java
@@ -6,8 +6,9 @@ import com.mci.ticketpilot.views.MainLayout;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -128,12 +129,22 @@ public class TicketListView extends VerticalLayout {
         service.saveTicket(event.getTicket());
         updateList();
         closeEditor();
+        // Benachrichtigung erstellen und anzeigen
+        Notification notification = new Notification("Ticket wurde erstellt!", 3000);
+        notification.setPosition(Notification.Position.BOTTOM_END);
+        notification.addThemeVariants(NotificationVariant.LUMO_PRIMARY);
+        notification.open();
     }
 
     private void deleteTicket(TicketForm.DeleteEvent event) {
         service.deleteTicket(event.getTicket());
         updateList();
         closeEditor();
+        // Benachrichtigung erstellen und anzeigen
+        Notification notification = new Notification("Ticket wurde gelöscht!", 3000);
+        notification.setPosition(Notification.Position.BOTTOM_END);
+        notification.addThemeVariants(NotificationVariant.LUMO_PRIMARY);
+        notification.open();
     }
 
     private Component getToolbar() {
