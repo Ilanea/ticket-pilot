915b7d88f5bb1491f1b4999bc45d642b1fed2763
Ilanea <Ilanea@users.noreply.github.com>
2023-06-13 14:22:18 +0200

    feat: kanban (#16)
    * Update banner.txt
    
    * bare project template
    
    * bare projects
    
    * added project managers
    
    * users in h2 db
    
    * added logging
    
    * admin login working
    
    * added users
    
    - role display not working as enum != string
    
    * Working Users and Security
    
    * cleanup
    
    * Update README.md
    
    * add ticket methods and rename ticketservice to pilotservice
    
    * remove unused stuff
    
    * rename to createUserButton
    
    * added ticketform
    
    * work on tickets
    
    not working, but atleast the objects show up
    
    * bit of work on project to ticket linking
    
    * working on ticket
    
    - editing a tickets project and user does not work
    - saving a ticket without a project and user also doesn't work
    
    * editing tickets works
    
    * cleanup
    
    * projects and users cannot be null
    
    * Update TicketForm.java
    
    * Update TicketForm.java
    
    * foo
    
    * only manager and admin can create projects
    
    * Email function added
    
    * Comments now in  Ticket
    
    * small fix to not create multiple comment displays
    
    * every user should see the comment section
    
    * New "Help" Button and some minor changes to style
    
    * New "Help" Button and some minor changes to style
    
    * New "Help" Button and some minor changes to style
    
    * Dhasboard adoptions
    
    * Dhasboard adoptions
    
    * Upload Document in Tickets
    
    * split document upload from ticketform
    
    * switch from mysql to postgresql
    
    * upload document to database and added download functionality
    
    * changes to comment and documents
    
    - the only changes we need to documents and comments is when a ticket gets deleted they also should get deleted, we don't need all operations on them
    
    * added download and delete buttons for documents
    
    * fix: ticketcreationdate should not get updatet after initial ticket creation
    
    * Dashboard updates with filters
    
    * Kanban Update
    
    ---------
    
    Co-authored-by: mario.bedenk <mario.bedenk@innio.com>
    Co-authored-by: heinz <mh2126@mci4me.at>
    Co-authored-by: Obanzi <heimay237@gmail.com>

---
+++ src/main/java/com/mci/ticketpilot/views/lists/KanbanView.java
@@ -0,0 +1,134 @@
+package com.mci.ticketpilot.views.lists;
+
+import com.mci.ticketpilot.data.entity.Ticket;
+import com.mci.ticketpilot.data.entity.TicketStatus;
+import com.mci.ticketpilot.data.service.PilotService;
+import com.mci.ticketpilot.views.MainLayout;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import jakarta.annotation.security.PermitAll;
+import org.springframework.context.annotation.Scope;
+
+import java.util.HashMap;
+import java.util.Map;
+@SpringComponent
+@Scope("prototype")
+@PermitAll
+@Route(value = "kanban", layout = MainLayout.class)
+@PageTitle("Kanban | Ticket Pilot")
+public class KanbanView extends VerticalLayout {
+    private PilotService service;
+    private Map<String, VerticalLayout> statusColumns;
+
+    public KanbanView(PilotService service) {
+        this.service = service;
+        addClassName("kanban-view");
+        setSizeFull();
+        this.statusColumns = new HashMap<>();
+
+        VerticalLayout colum1 = createStatusColumn("OPEN");
+        colum1.addClassName("kanban-column");
+        colum1.setWidth("250px");
+        colum1.setHeight("1300px");
+        colum1.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+        VerticalLayout colum2 = createStatusColumn("IN_PROGRESS");
+        colum2.addClassName("kanban-column");
+        colum2.setWidth("250px");
+        colum2.setHeight("1300px");
+        colum2.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+        VerticalLayout colum3 = createStatusColumn("RESOLVED");
+        colum3.addClassName("kanban-column");
+        colum3.setWidth("250px");
+        colum3.setHeight("1300px");
+        colum3.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+        VerticalLayout colum4 = createStatusColumn("CLOSED");
+        colum4.addClassName("kanban-column");
+        colum4.setWidth("250px");
+        colum4.setHeight("1300px");
+        colum4.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+        VerticalLayout colum5 = createStatusColumn("REOPENED");
+        colum5.addClassName("kanban-column");
+        colum5.setWidth("250px");
+        colum5.setHeight("1300px");
+        colum5.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+        VerticalLayout colum6 = createStatusColumn("ON_HOLD");
+        colum6.addClassName("kanban-column");
+        colum6.setWidth("250px");
+        colum6.setHeight("1300px");
+        colum6.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+        HorizontalLayout kanbanLayout = new HorizontalLayout(colum1, colum2, colum3, colum4, colum5, colum6);
+
+        statusColumns.put("OPEN", colum1);
+        statusColumns.put("IN_PROGRESS", colum2);
+        statusColumns.put("RESOLVED", colum3);
+        statusColumns.put("CLOSED", colum4);
+        statusColumns.put("REOPENED", colum5);
+        statusColumns.put("ON_HOLD", colum6);
+
+        add(kanbanLayout);
+
+        updateView();
+    }
+
+    private VerticalLayout createStatusColumn(String status) {
+        VerticalLayout statusColumn = new VerticalLayout();
+        statusColumn.addClassName("status-column");
+        statusColumn.add(status); // You'd want to use a Label or similar here
+        return statusColumn;
+    }
+
+    private void updateView() {
+        for (Ticket ticket : service.findAllTickets()) {
+            TicketStatus status = ticket.getTicketStatus();
+            // Convert the status to a string
+            String statusString = status.name();
+            VerticalLayout column = statusColumns.get(statusString);
+
+            if (column != null) {
+                Component ticketComponent = createTicketComponent(ticket);
+                column.add(ticketComponent);
+            } else {
+                System.out.println("Could not find column for status " + statusString);
+            }
+        }
+    }
+
+
+    private Component createTicketComponent(Ticket ticket) {
+        VerticalLayout ticketDiv = new VerticalLayout();
+        ticketDiv.addClassName("ticket");
+
+        Label ticketNameLabel = new Label("Name: " + ticket.getTicketName() + "\n");
+        Label ticketDescriptionLabel = new Label("Description: " + ticket.getTicketDescription() + "\n");
+        Label ticketPriorityLabel = new Label("Priority: " + ticket.getTicketPriority() + "\n");
+        Label ticketAssigneeLabel = new Label("Assignee: " + ticket.getAssignee() + "\n");
+
+        ticketNameLabel.addClassName("ticket-name");
+        ticketDescriptionLabel.addClassName("ticket-description");
+        ticketPriorityLabel.addClassName("ticket-priority");
+        ticketAssigneeLabel.addClassName("ticket-assignee");
+
+        ticketDiv.add(ticketNameLabel, ticketDescriptionLabel, ticketPriorityLabel, ticketAssigneeLabel);
+
+        ticketDiv.addClickListener(event -> openTicket());
+
+        return ticketDiv;
+    }
+
+    private void openTicket() {
+        UI.getCurrent().navigate(TicketListView.class);
+    }
+}
